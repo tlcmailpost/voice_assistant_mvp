1924885826ed1b69c0497835ff6143bbc6f9c84e
tlcmailpost <tlcmailpost@gmail.com>
2025-09-07 00:37:07 -0400

    Delete twilio_response.py

--- twilio_response.py
+++
@@ -1,6 +0,0 @@
-from twilio.twiml.voice_response import VoiceResponse
-
-def create_twiml_response(text):
-    response = VoiceResponse()
-    response.say(text, voice='alice', language='ru-RU')
-    return str(response)
